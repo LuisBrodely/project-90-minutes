74f5d86a5bbd5c925daaefa114236f23756a24c2
IsaiGonzalez08 <isaigonzalez482@gmail.com>
2024-06-23 22:51:36 -0600

    [FIX] dashboard page CardPackage ButtonCardPackage dashboard

--- src/app/dashboard/(dashboard)/package/components/CardPackage.tsx
+++ src/app/dashboard/(dashboard)/package/components/CardPackage.tsx
@@ -1,15 +1,24 @@
 import Image from "next/image";
 import box from "@/assets/img/box.png";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+
 import { ButtonCardPackage } from "./ButtonCardPackage";
 
 interface CardPackageProps {
-  name: string;
+  title: string;
 }
 
-export const CardPackage = ({ name }: CardPackageProps) => {
+export const CardPackage = ({ title }: CardPackageProps) => {
   return (
-    <div className="w-full h-80 shadow-md mt-5">
-      <div className="w-full h-20 bg-[#F3EEFC] flex justify-between items-center">
+    <Card className="w-full h-80 shadow-md mt-5">
+      <CardHeader className="w-full h-20 bg-[#F3EEFC] flex flex-row justify-between">
         <div className="flex gap-10 ml-10">
           <div className="flex flex-col">
             <h2 className="text-[#686868] font-normal text-sm">
@@ -35,13 +44,13 @@ export const CardPackage = ({ name }: CardPackageProps) => {
         <div className="mr-10 text-[#686868] font-semibold text-xs">
           <h2>ID {"[ID del Paquete]"}</h2>
         </div>
-      </div>
-      <div className="w-full h-[240px] bg-[#FFF] flex justify-between pt-5">
+      </CardHeader>
+      <CardContent className="w-full h-[240px] bg-[#FFF] flex justify-between pt-5">
         <div className="flex flex-col ml-10">
-          <h2 className="text-[#7C3AED] font-semibold text-xl">{name}</h2>
-          <h2 className="text-[#939393] font-normal text-sm">
+          <CardTitle className="text-[#7C3AED] font-semibold text-xl">{title}</CardTitle>
+          <CardDescription className="text-[#939393] font-normal text-sm">
             El paquete ha sido entregado al destino correspondiente.
-          </h2>
+          </CardDescription>
           <div className="flex items-center gap-5 mt-5">
             <Image src={box} alt="Img-Box" />
             <div className="flex flex-col">
@@ -57,7 +66,7 @@ export const CardPackage = ({ name }: CardPackageProps) => {
           <ButtonCardPackage name="Realizar seguimiento de paquete" />
           <ButtonCardPackage name="Escribir opinión del servicio" />
         </div>
-      </div>
-    </div>
+      </CardContent>
+    </Card>
   );
 };
